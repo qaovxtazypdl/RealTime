bf9288c84e333ac5b7cc65ce99c62c5c8af5c0a7
Raheman Vaiya <rvaiya@uwaterloo.ca>
2017-03-13 15:16:13 -0400

    wip

--- src/user/train/train.c
+++ src/user/train/train.c
@@ -26,12 +26,20 @@ static void update_position(struct track_node **sensors, int *velocity, int last
   //TODO implement.
 }
 
+static void update_stopping_time(struct track_node **sensors,
+                                 int *stopping_time,
+                                 int velocity,
+                                 struct position position,
+                                 struct track_node **path) {
+}
+
 void train() {
   int td;
   int num, i;
   int velocity;
   int direction;
   int last_update;
+  int stopping_time;
   struct track_node **c;
 
   struct position position;
@@ -44,6 +52,7 @@ void train() {
   } msg;
 
   int sens_td = sensor_subscribe();
+  int delay_td = -1;
   
   last_update = get_time();
   while(1) {
@@ -52,7 +61,11 @@ void train() {
     if(td == sens_td) {
       update_velocity(msg.sensors, &velocity, last_update);
       update_position(msg.sensors, &position, last_update);
+      update_stopping_time(msg.sensors, &stopping_time, velocity, position, struct track_node **path);
       last_update = get_time();
+      delay_td = delay_async(async, time);
+    } else if(td == delay_td) {
+      //CHECK stopping time against current time and stop if equal.
     } else {
       switch(msg.command.type) {
         case TRAIN_COMMAND_SET_PATH:
